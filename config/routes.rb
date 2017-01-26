a8096b2289e1dc0ee009dc7961ebadea986e2fa3
Daniel Chinedu <chinedudaniel7@gmail.com>
2017-01-26 18:29:11 +0100

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  root 'links#index'
+
+  resources :links, except: :index do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+    post :upvote, on: :member
+    post :downvote, on: :member
+  end
+
+  get '/comments' => 'comments#index'
+  get '/newest' => 'links#newest', as: :newest_links
+
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
+
+  resources :users, only: [:new, :create]
+end
